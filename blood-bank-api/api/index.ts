71f2ba6267e419e3008f47ffbd3a58ca7153a156
Godzap <enzo.baiao1@hotmail.com>
2026-01-06 15:11:47 -0300

    pofavor

--- blood-bank-api/api/index.ts
+++ blood-bank-api/api/index.ts
@@ -13,13 +13,13 @@ async function bootstrap() {
 
             app.enableCors({
                 origin: [
-                    'https://hemo-rastreio-front.vercel.app/login',
                     'https://hemo-rastreio-front.vercel.app',
                     'http://localhost:5173',
                     'http://localhost:3000',
                     process.env.FRONTEND_URL,
                 ].filter(Boolean),
                 credentials: true,
+                optionsSuccessStatus: 200,
                 methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
                 allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
             });
