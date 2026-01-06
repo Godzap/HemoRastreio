64dff4c763d1a02589b0551bd3362adada934c05
Godzap <enzo.baiao1@hotmail.com>
2026-01-06 14:18:00 -0300

    agoravaicemporcento

--- blood-bank-api/src/auth/auth.module.ts
+++ blood-bank-api/src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './strategies';
             useFactory: async (configService: ConfigService) => ({
                 secret: configService.get<string>('JWT_SECRET'),
                 signOptions: {
-                    expiresIn: configService.get<string>('JWT_EXPIRATION', '15m'),
+                    expiresIn: configService.get<string>('JWT_EXPIRATION', '15m') as any,
                 },
             }),
             inject: [ConfigService],
